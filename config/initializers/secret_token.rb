290f3fc9e09d26d18a9c20ec67db5a29fd07d16e
Jon Font <font@venmo.com>
2012-06-26 17:45:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterApp::Application.config.secret_token = 'cea68c73bc64ee2e2ae4ae355e06c6d95bcc34edbc35bf1a3f46912b06945b52a95908f6c56d1dacaf0a1469eefc16305ec686eb4b9d22c4cc7a4fb8dd109daf'
