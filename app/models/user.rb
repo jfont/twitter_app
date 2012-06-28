9742879ad336c99adc5354503be122767ad68583
Jon Font <font@venmo.com>
2012-06-28 16:39:19 -0400

    Add a full_title helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  def initialize (attributes = {})
+    @name = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
 end
